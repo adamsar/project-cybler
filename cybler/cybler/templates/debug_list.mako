9f91d5abdaad0f07ace63b2caa80498fc51cbf9b
Andrew Adams <andy@Andrews-MacBook-Pro.local>
2013-05-17 13:37:24 +0900

    Add in adultsearch celery task

--- cybler/cybler/templates/debug_list.mako
+++ cybler/cybler/templates/debug_list.mako
@@ -6,7 +6,24 @@
   Results: <br/>
   <ul>
   % for entry in listings:
-    <li>${entry["title"]}</li>
+    <li>
+      <a href="${entry["link"]}">${entry["title"]}</a>
+      <hr/>
+      <div>
+        ${entry["description"]}
+      </div>
+      <div style="color: red;">
+        From: ${entry["type"]}
+      </div>
+      <div>
+        Images:
+        <ul>
+          % for image in entry["images"]:
+            <li><img src="image"/></li>
+          % endfor
+        </ul>
+      </div>
+    </li>
   % endfor
   </ul>
   </body>
--- cybler_tasks/cybler_tasks/tasks/scraped_ingestion.py
+++ cybler_tasks/cybler_tasks/tasks/scraped_ingestion.py
@@ -7,6 +7,7 @@ Tasks for ingestion done by web scraping. For the moment this includes:
 import os.path
 from celery import task
 from cybler_tasks.data import feed_scraper
+ADULTSEARCH_FILE = os.path.join(os.path.dirname(__file__), "../../static/adultsearch_sources.txt")
 CRAIGS_LIST_FILE = os.path.join(os.path.dirname(__file__), "../../static/craigslist_sources.txt")
 BACKPAGE_LIST_FILE = os.path.join(os.path.dirname(__file__), "../../static/backpage_sources.txt")
 
@@ -33,3 +34,11 @@ def ingest_craigslist():
         feed_scraper.process_craigslist.delay(url, city, state)
 
     
+@task
+def ingest_adultsearch():
+    """Ingests all adultsearch entries"""
+    data = open(ADULTSEARCH_FILE).read()
+    entries = data.split("\n")
+    for entry in entries:
+        url, state, city = entry.split(",")
+        feed_scraper.process_adultsearch.delay(url, city, state)
