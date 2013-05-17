fdfab9275f0d9e6de24ede23a0e30dfe5c49d580
Andrew Adams <andy@Andrews-MacBook-Pro.local>
2013-05-17 13:39:00 +0900

    proper imports

--- cybler_tasks/celeryconfig.py
+++ cybler_tasks/celeryconfig.py
@@ -5,7 +5,7 @@ setup_app("stage.ini")
 
 BROKER_URL = "mongodb://localhost:27017/cybler"
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ("celery_tasks.tasks.scraped_ingestion", "celery_tasks.data.feed_scraper")
+CELERY_IMPORTS = ("cybler_tasks.tasks.scraped_ingestion", "cybler_tasks.data.feed_scraper")
 
 CELERYBEAT_SCHEDULE = {
     "ingest-backpage": {
